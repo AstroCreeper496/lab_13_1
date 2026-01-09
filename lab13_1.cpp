0aac27fc2d6cd7761882ff87fcfb31946c0e8202
AstroCreeper496 <pluccoqlu.1927@gmail.com>
2026-01-09 15:22:32 +0700

    Update lab13_1.cpp

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,41 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double a[], int b, double c[]){
+    double sum = 0;
+    double sumofsquares = 0;
+    double prod = 1;
+    double HarmoSum = 0;
+    double Min = a[0];
+    double Max = a[0];
+    for (int i = 0; i < b; i++) {
+        sum += a[i];
+        sumofsquares += a[i] * a[i];
+        prod *= a[i];
+        HarmoSum += 1.0 / a[i];
+        if (a[i] > Max) Max = a[i];
+        if (a[i] < Min) Min = a[i];
+    }
+    
+    double ArithMean = sum / b;
+    
+    double stvar = 0;
+        for (int i = 0; i < b; i++) {
+            stvar += (a[i] - ArithMean) * (a[i] - ArithMean);
+    }
+    
+    double stdev = sqrt(stvar / b);
+    
+    double GeoMean = 0;
+    GeoMean = pow(prod, 1.0 / b);
+    
+    double HarmoMean = 0;
+    HarmoMean = double(b / HarmoSum);
+    c[0] = ArithMean;
+    c[1] = stdev;
+    c[2] = GeoMean;
+    c[3] = HarmoMean;
+    c[4] = Max;
+    c[5] = Min;
+}
